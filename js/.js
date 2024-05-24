a41222e475f9d112f62b9b4b17cab0cd331e301d
Estrella <eingalla@hotmail.com>
2024-05-23 21:33:07 -0500

    agregandologica

---
+++ js/.js
@@ -0,0 +1,44 @@
+
+ // let mensaje = [17,15,19];
+
+  //  let suma = 0;
+  //  for (i in mensaje) {
+  //  suma = suma + mensaje[i];
+
+   // if (i == mensaje.length - 1) {
+  //  suma = suma/ mensaje.length;
+ //   }
+//console.log(suma)
+//} 
+
+//let mensaje = [17,15,19,5];
+//let i=0;
+//let suma = 0;
+
+//while (i < mensaje.length) {
+//suma = suma + mensaje[i];
+//console.log(suma)
+
+//if (i == mensaje.length-1) {
+//suma= suma/mensaje.length;
+//console.log("Promedio",suma)
+//}
+
+//i++;
+//}
+let cantidad = [i];
+let i=0;
+let desc = 0;
+
+while (i < 10) {
+desc = 0.035*= mensaje[i];
+console.log(suma)
+
+if (i == mensaje.length-1) {
+suma= suma/mensaje.length;
+console.log("Promedio",suma)
+}
+
+i++;
+}
+
